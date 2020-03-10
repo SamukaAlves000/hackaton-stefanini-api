93f418c168b6f0377720a78975ad07b212e3c27e
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2020-03-09 23:48:34 -0300

    Criando Métodos com CriteriaBuilder
    - Buscar pessoas de determinada UF.
    - Buscar pessoas por situacao.

--- src/main/java/com/stefanini/servico/PessoaServico.java
+++ src/main/java/com/stefanini/servico/PessoaServico.java
@@ -1,6 +1,7 @@
 package com.stefanini.servico;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,9 +11,16 @@ import javax.ejb.TransactionAttributeType;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.inject.Inject;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.validation.Valid;
 
 import com.stefanini.dao.PessoaDao;
+import com.stefanini.model.Endereco;
 import com.stefanini.model.Pessoa;
 
 /**
@@ -71,6 +79,48 @@ public class PessoaServico implements Serializable {
 	 */
 	public Optional<Pessoa> encontrar(Long id) {
 		return dao.encontrar(id);
+	}
+	
+	/**
+	 * Buscar pessoas por situacao 
+	 */
+	public List<Pessoa> encontrar(boolean situacao) {
+		
+		CriteriaBuilder builder = dao.getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Pessoa> criteria = builder.createQuery(Pessoa.class);
+		Root<Pessoa> root = criteria.from(Pessoa.class);
+		
+		criteria.where(root.get("situacao").in(situacao));
+		TypedQuery<Pessoa> query = dao.getEntityManager().createQuery(criteria);
+		
+		return query.getResultList();
+	}
+	
+	
+	/**
+	 * Buscar pessoas de determinada UF
+	 */
+	public List<Pessoa> encontrar(String uf) {
+		
+		CriteriaBuilder builder = dao.getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Pessoa> criteria = builder.createQuery(Pessoa.class);
+		Root<Pessoa> root = criteria.from(Pessoa.class);
+		
+		List<Predicate> predicateList = new ArrayList<Predicate>();
+		
+		Join<Pessoa, Endereco> join = root.join("enderecos");
+		Predicate predEnd = criteria.where(join.get("uf").in(uf)).getRestriction();
+		
+		predicateList.add(predEnd);
+		
+		Predicate[] predicates = new Predicate[predicateList.size()];
+		predicateList.toArray(predicates);
+		criteria.where(predicates);
+		 
+		TypedQuery<Pessoa> query = dao.getEntityManager().createQuery(criteria);
+		
+		return query.getResultList();
+		
 	}
 
 }
